9d6c20b696b18043d85ffa2fb50ea7b0b00318ba
daniel <daniel2mf@yahoo.com.br>
2025-05-09 01:29:35 -0300

    imagens do projeto

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-// src/App.jsx
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Products from "./components/Products";
 
 export default function App() {
   return (
     <>
       <Header />
       <Hero />
+      <Products />
     </>
   );
 }
