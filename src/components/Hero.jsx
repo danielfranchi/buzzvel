ca85b0b7a3646c6c809b84b73c53fb1fc89fb76c
daniel <daniel2mf@yahoo.com.br>
2025-05-10 02:45:14 -0300

    componente Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,20 +11,26 @@ import PlayCircle from "../assets/play-circle.svg";
 import StudentImg from "../assets/student.svg";
 import TeacherImg from "../assets/teacher.svg";
 
+// eslint-disable-next-line no-unused-vars
+import { motion } from "framer-motion";
+
 export default function Hero() {
   return (
     <section className="px-4 py-10 md:px-8 md:py-20 bg-white">
       <div className="w-full max-w-[1440px] mx-auto grid grid-cols-1 md:grid-cols-2 items-center gap-8">
-        <div className="relative text-left  pl-4 md:pl-0">
-          <h1 className="text-[#0F172A] font-roboto font-extrabold text-4xl md:text-5xl lg:text-6xl leading-[110%]">
-            Teach students <br />
-            <span className="inline-block">worldwide</span>
-          </h1>
+        <div className="relative text-left pl-4 md:pl-0">
+          <header>
+            <h1 className="text-[#0F172A] font-roboto font-extrabold text-4xl md:text-5xl lg:text-6xl leading-[110%]">
+              Teach students <br />
+              <span className="inline-block">worldwide</span>
+            </h1>
+          </header>
 
           <img
             src={Grifo}
             alt="Grifo"
             className="absolute h-auto w-24 left-4 top-8 sm:left-[10px] sm:top-[50px] md:w-32 lg:w-40 md:left-[1%] md:top-[11%]"
+            loading="lazy"
           />
 
           <p className="text-[#0F172A] font-roboto text-lg md:text-xl lg:text-2xl leading-[160%] mt-8">
@@ -40,17 +46,32 @@ export default function Hero() {
           </p>
 
           <div className="relative flex flex-row items-center gap-6 mt-10 md:flex-row md:pl-0">
-            <Button variant="orange">Sign Up Now</Button>
+            <motion.div
+              whileHover={{ scale: 1.1 }}
+              transition={{ duration: 0.3 }}
+            >
+              <Button variant="orange">Sign Up Now</Button>
+            </motion.div>
 
-            <div className="flex items-center gap-2">
-              <img src={PlayCircle} alt="PlayCircle" className="h-auto" />
+            <motion.div
+              className="flex items-center gap-2"
+              initial={{ scale: 1 }}
+              whileHover={{ scale: 1.1 }}
+              transition={{ duration: 0.3 }}
+            >
+              <img
+                src={PlayCircle}
+                alt="PlayCircle"
+                className="h-auto"
+                loading="lazy"
+              />
               <a
                 href="#demo"
                 className="text-[#2563EB] font-roboto font-medium text-lg"
               >
                 View Demo
               </a>
-            </div>
+            </motion.div>
           </div>
 
           <div className="mt-18 flex flex-col items-start gap-4 text-[#475569] md:pl-0 md:flex-row">
@@ -63,47 +84,62 @@ export default function Hero() {
                 src={IconsCollection1}
                 alt="Icon 1"
                 className="w-[25.21px] h-[32px]"
+                loading="lazy"
               />
               <img
                 src={IconsCollection2}
                 alt="Icon 2"
                 className="w-[25.21px] h-[32px]"
+                loading="lazy"
               />
               <img
                 src={IconsCollection3}
                 alt="Icon 3"
                 className="w-[25.21px] h-[32px]"
+                loading="lazy"
               />
               <img
                 src={IconsCollection4}
                 alt="Icon 4"
                 className="w-[25.21px] h-[32px]"
+                loading="lazy"
               />
               <img
                 src={IconsCollection5}
                 alt="Icon 5"
                 className="w-[25.21px] h-[32px]"
+                loading="lazy"
               />
             </div>
           </div>
         </div>
 
         <div className="relative w-full h-[500px] flex justify-center items-center right-10">
-          <img
+          <motion.img
             src={TeacherImg}
             alt="Teacher"
             className="absolute w-64 h-auto scale-90 top-[40%] left-[35%] transform -translate-x-1/2"
+            initial={{ opacity: 0, x: -50 }}
+            whileInView={{ opacity: 1, x: 0 }}
+            transition={{ duration: 0.6 }}
+            viewport={{ once: false, amount: 0.3 }}
           />
 
           <img
             src={ChatImg}
             alt="Chat"
             className="absolute w-32 md:w-48 lg:w-56 h-auto top-[10%] left-[50%] transform -translate-x-1/2"
+            loading="lazy"
           />
-          <img
+
+          <motion.img
             src={StudentImg}
             alt="Student"
             className="absolute w-32 md:w-48 lg:w-56 h-auto top-[10%] left-[80%] transform -translate-x-1/2"
+            initial={{ opacity: 0, x: 50 }}
+            whileInView={{ opacity: 1, x: 0 }}
+            transition={{ duration: 1.2 }}
+            viewport={{ once: false, amount: 0.3 }}
           />
         </div>
       </div>
