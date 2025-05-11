4bf743a21de6f4bd456044bab8a7693b45a906bd
daniel <daniel2mf@yahoo.com.br>
2025-05-11 06:10:06 -0300

    componente inicial da Lading Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React, { Suspense, lazy } from "react";
-import MeetSection from "./components/MeetSection";
-import StatisticsSection from "./components/StatisticsSection";
 
 const Header = lazy(() => import("./components/Header"));
 const Hero = lazy(() => import("./components/Hero"));
 const Products = lazy(() => import("./components/Products"));
 const TestimonialCard = lazy(() => import("./components/TestimonialCard"));
+const MeetSection = lazy(() => import("./components/MeetSection"));
+const StatisticsSection = lazy(() => import("./components/StatisticsSection"));
 
 export default function App() {
   return (
